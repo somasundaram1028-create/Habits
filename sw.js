68c5cbc4fd7421b7954c249caf9f0f0c3e9114dc
somasundaram1028-create <somasundaram1028@gmail.com>
2026-08-21 12:45:50 +0530

    sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'do-it-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn-icons-png.flaticon.com/512/7518/7518748.png'
+];
+
+// Install Event - Cache Files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate Event - Clean old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch Event - Serve from Cache when Offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
